8c9883d9f58e805f9045a9b2c1c57bafc040271d
priya-abc <priyapriya6598@gmail.com>
2020-11-10 18:52:41 +0530

    Added: Employee Attendance checking Implemented

---
+++ employee.java
@@ -0,0 +1,30 @@
+package emp;
+import java.util.*;
+public class employee {
+
+        public static void main(String [] args) {
+		int WagePerHour=20;
+		int FullDayHour=8;
+		int HalfDayHour=4;
+		int wage=0;
+		int hours=0;
+		int days=0;
+		int totalhours=0;
+		int totalwage=0;
+		System.out.println("Welcome to Employee Wage Computation Program");
+		Random rnum = new Random();
+		while( days!=20 && totalhours!=100)
+		{
+		int random = rnum.nextInt(2);
+		int halfday= rnum.nextInt(2);
+		days++;
+		totalhours=totalhours+hours;
+		switch (random)
+		{case 0:
+			System.out.println("Absent");
+			wage=0;
+			hours=0;
+			break;
+		default:
+			System.out.println("Invalid Entry");
+		}
