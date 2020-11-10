2e424d681255ced2c23d6be27730d96c0be5f2f3
priya-abc <priyapriya6598@gmail.com>
2020-11-10 19:09:38 +0530

    Added:parttimewage added

--- employee.java
+++ employee.java
@@ -20,11 +20,28 @@ public class employee {
 		days++;
 		totalhours=totalhours+hours;
 		switch (random)
-		{case 0:
+		{
+                 case 1:
+			System.out.println("Present");
+			if ( halfday == 0)
+			{
+				System.out.println("Employee present for half day only");
+				wage=WagePerHour*HalfDayHour;
+				hours=4;
+			}
+			else
+			{
+				System.out.println("Employee present for full day");
+				wage=WagePerHour*FullDayHour;
+				hours=8;
+			}
+			break;
+                 case 0:
 			System.out.println("Absent");
 			wage=0;
 			hours=0;
 			break;
-		default:
+		
+                 default:
 			System.out.println("Invalid Entry");
 		}
