e449f5fc03942e997497f92535a542343e3bfbc5
priya-abc <priyapriya6598@gmail.com>
2020-11-21 13:24:00 +0530

    Added: Array List added

--- employee.java
+++ employee.java
@@ -6,94 +6,47 @@ public class EmployeeDetail {
 		String name;
 		System.out.println("Welcome to Employee Wage Computation Program");
 		import java.util.*;
+public class paycheck {
 
-class EmployeeDetail {
-=======
-		System.out.println("Welcome to Employee Wage Computation Program");
->>>>>>> UC8-modifying
-	private String name;
-	private float salary, hours;
-	
-	public EmployeeDetail() {
-		name = " ";
-		salary = 0;
-		hours = 0;
-	}
-	
-	public void getInfo(String n, float sal, float hr) {
-		name = n;
-		salary = sal;
-		hours = hr;
-	}
-	
-	public float AddSal() {
-		if(salary<500) {
-			salary = salary + 10;
-		}
-		return salary;
-	}
-	
-	public float AddWork() {
-		if(hours > 6) {
-			salary = salary + 5;
-		}
-		return salary;
-	}
-}
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
 
-class EmployeeTester
-{
-	public static void main(String [] args)
-	{
-		// Ask user for number of employees; create array of appropriate size
-		
-		int numberOfEmployees = Integer.parseInt(
-		Employee [] employees = new Employee [numberOfEmployees];
-		
-		// Read information on individual employees.  In each case, first ask
-		// user what kind of employee, then pop up dialog to read relevant
-		// information
-		
-		String [] employeeKinds = { "Hourly", "Salaried" };
-		String [] hourlyLabels = { "Name", "SSN", "Hourly Rate" };
-		String [] salariedLabels = { "Name", "SSN", "Annual Salary" };
+        Scanner input = new Scanner (System.in);
 
-		for (int i = 0; i < numberOfEmployees; i ++)
-		{
-			String kind = (String) JOptionPane.showInputDialog(null,
-				"What kind of employee is employee " + (i+1) + "? ",
-				"", JOptionPane.QUESTION_MESSAGE, null, employeeKinds, "Hourly");
-			if (kind.equals("Hourly"))
-			{
-				String [] employeeInfo = MultiInputPane.showMultiInputDialog(
-					null, hourlyLabels, "Employee Information");
-				employees[i] = new HourlyEmployee(employeeInfo[0], 
-					employeeInfo[1], Double.parseDouble(employeeInfo[2]));
-			}
-			else				
-			{
-				String [] employeeInfo = MultiInputPane.showMultiInputDialog(
-					null, salariedLabels, "Employee Information");
-				employees[i] = new SalariedEmployee(employeeInfo[0], 
-					employeeInfo[1], Double.parseDouble(employeeInfo[2]));
-			}
-		}
-				
-		// Print out information on all the employees
-		
-		System.out.println("Information on employees:"); System.out.println();
-		for (int i = 0; i < employees.length; i ++)
-			System.out.println(employees[i].toString());
-	  
-		// Pay them.  Note that, for each hourly employee, the user will be
-		// asked to input the hours worked
-		
-		System.out.println();
-		System.out.println("Weekly payroll:"); System.out.println();
-		for (int i = 0; i < employees.length; i ++)
-			System.out.println(employees[i].getName() + " $" + 
-				(new DecimalFormat("0.00")).format(employees[i].weeklyPay()));
-				
-		System.exit(0);
-	}
+        NumberFormat nf = NumberFormat.getCurrencyInstance();
+
+        System.out.println("Enter the employee name: ");
+        long empName = input.nextLong();
+
+        mediaArray[empName];
+
+        System.out.println("Enter your total sales for the year:  ");
+        double totalSales = input.nextDouble();
+
+        System.out.println("\r");
+
+        Calculations c = new Calculations (totalSales);
+
+        System.out.println("Your Total compensation with your annual sales is:  " + nf.format(c.getCommissionCalc()));
+
+        System.out.println("\r");
+
+        System.out.println("If you were to increase your sales you could earn even more money!");
+
+        System.out.println("\r");
+
+        double i = totalSales + 5000;
+        double finish = totalSales * 1.5;
+
+        while (i <= finish) {
+
+            c.totalSales = i;
+
+            System.out.println("If you were to increase your sales commission to " + nf.format(i) + " you could earn: " + nf.format(c.getCommissionCalc()));
+
+            i = i + 5000;
+        }
+
+    }
+
 }
